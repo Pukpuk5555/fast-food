2a849e6922392e858a0552b9c9fc8b1b460f8bd0
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-04-19 20:43:35 +0700

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ public class Player : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Animator animator;
 
+    [SerializeField]
+    private Sprite healthySprite, fatSprite, superFatSprite;
+
+    [SerializeField]
+    private RuntimeAnimatorController healthyAnim, fatAnim, superFatAnim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

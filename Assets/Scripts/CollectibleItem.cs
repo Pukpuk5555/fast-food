6f61e6e307f7943ec2be545d40689e4971c2146e
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-04-19 14:12:30 +0700

    Fat & collect items

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+public enum ItemType
+{
+    Salad,
+    Weight,
+    Burger,
+    Cola
+}
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] ItemType itemType;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerStats player = other.GetComponent<PlayerStats>();
+
+        if(player != null)
+        {
+            switch (itemType)
+            {
+                case ItemType.Salad:
+                    player.ReduceFat(10);
+                    break;
+                case ItemType.Weight:
+                    player.ReduceFat(5);
+                    break;
+                case ItemType.Burger:
+                    player.IncreaseFat(20);
+                    break;
+                case ItemType.Cola:
+                    player.IncreaseFat(10);
+                    break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private int fatValue = 100;
+    [SerializeField] private TextMeshProUGUI fatValueText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateUI()
+    {
+        fatValueText.text = "Fat: " + fatValue.ToString();
+    }
+
+    public void ReduceFat(int fat)
+    {
+        fatValue -= fat;
+        //fatValue = Mathf.Max(fatValue, 0);
+        UpdateUI();
+    }
+
+    public void IncreaseFat(int fat)
+    {
+        fatValue += fat;
+        UpdateUI();
+    }
+}
